cff29fb5fe57f782afdeaf12cf1d52c6a6b74497
ChiesiMario <ChiesiMario@users.noreply.github.com>
2026-06-25 00:49:28 +0800

    chore: cleanup python scripts

--- fix_nr_2.py
+++
@@ -1,7 +0,0 @@
-with open('frontend/src/components/NodeRanking.tsx', 'r', encoding='utf-8') as f:
-    nr = f.read()
-
-nr = nr.replace("{ nodeName, safeId, avgDelay, jitter, score }", "{ nodeName, avgDelay, jitter, score }")
-
-with open('frontend/src/components/NodeRanking.tsx', 'w', encoding='utf-8') as f:
-    f.write(nr)
